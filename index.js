c9bf0f9cf96fc8410a92d7e976a65d9cb9f3e8cb
Conrad Mitchell <t-comitc@microsoft.com>
2019-08-04 18:19:29 -0400

    hello

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const bodyPasrer = require("body-parser");
+const JSON = require("JSON");
+const requests = require("request");
+var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+const PORT = process.env.PORT || 8000;
+
+var app = express();
+app.use(bodyPasrer.urlencoded({extended: false}));
+app.use(bodyPasrer.json());
+
+
+console.log("server started on 8000");
+app.listen(8000);
+
+app.post('/rm', (req, res)=>{
+		 console.log("time query");
+		 console.log(req.body); 
+		var bot_id = "2fd6b85077d8298aa71ac2f2c3"
+
+		let user = req.session.user;
+		let body = "bot_id="+ bot_id +  "&text=The Lodge"
+        
+	
+        let message = JSON.stringify(req.body).toLowerCase();
+	    if(message.indexOf("section") !== -1){
+      		let oReq = new XMLHttpRequest();
+      		oReq.open("POST", "https://api.groupme.com/v3/bots/post?bot_id="+bot_id+"&text=*The Lodge",true);
+      		oReq.onload = function(){console.log(oReq.response);}
+    		oReq.send(body);
+	    }
+
+
+		
+});
